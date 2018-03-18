593783d983195dc839333b27ba24f860ddf6c629
Roy DevAdmin <Roy.DevAdmin@caa.co.uk>
2018-03-18 14:28:24 +0000

    Run for whole responses (sentiment + key phrases ~1000 responses

--- textAnalytics.py
+++ textAnalytics.py
@@ -6,7 +6,8 @@ import requests
 from pprint import pprint
 
 text_analytics_base_url = "https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/"
-subscription_key = "56957c0c36d84eec8dfbef92cda9a57a"
+#subscription_key = "56957c0c36d84eec8dfbef92cda9a57a" #CP subscription key
+subscription_key = "62b509ed027a4fb884cb2969bbc72756" #Visual Studio Pro subscription (VR)
 sentiment_api_url = text_analytics_base_url + "sentiment"
 #sentiment_api_url = text_analytics_base_url + "keyPhrases"
 print(sentiment_api_url)
@@ -27,15 +28,24 @@ with open(inputFilePath) as csvFile:
 	#rows = list(reader)	
 	for row in reader:
 		data = {}
+		
 		count += 1
+		text = row[12].strip()
+		if not text:
+			continue
+		
+		
 		data['id'] = str(count)
 		data['language'] = 'en'
-		data['text'] = row[12]
+		data['text'] = text
 		list.append(data)
-		if count > 9:
+		#if count > 500:
 			#print(row[12])			
-			break;
+			#break;
 
+print('number of non empty records')
+print(count)		
+			
 documents = {}
 documents['documents'] = list
 	
@@ -56,9 +66,9 @@ documents['documents'] = list
 headers   = {"Ocp-Apim-Subscription-Key": subscription_key}
 response  = requests.post(sentiment_api_url, headers=headers, json=documents)
 sentiments = response.json()
-pprint(sentiments)
+#pprint(sentiments)
 
-outputFilePath = dir + "/sentiments_output.json"
-#outputFilePath = dir + "/key_phrases_output.json"
-with open(outputFilePath, "w") as outputfile:
+outputJsonPath = dir + "/sentiments_output.json"
+#outputJsonPath = dir + "/key_phrases_output.json"
+with open(outputJsonPath, "w") as outputfile:
 	json.dump(sentiments, outputfile)
